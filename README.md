fba7474d0abe2823c7eb0cea093b209817b43373
Bahtiar `kalkin` Gadimov <bahtiar@gadimov.de>
2022-04-21 19:19:38 +0200

    change: Remove `always_stderr` support

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # README
 
-A logger which logs to `STDERR` by default but to `STDOUT` on `info!()`.
+A logger which logs to `STDERR` by default but to `STDOUT` on `info!()` and
+`warn!()`.
--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,9 @@ use log::{Level, LevelFilter, Log, Metadata, Record};
 
 use std::io::{self, Write};
 
-static LOGGER: CliLog = CliLog {
-    always_stderr: false,
-};
+static LOGGER: CliLog = CliLog;
 
-static STDERR_LOGGER: CliLog = CliLog {
-    always_stderr: true,
-};
-
-struct CliLog {
-    always_stderr: bool,
-}
+struct CliLog;
 
 #[allow(clippy::print_stdout, clippy::print_stderr)]
 impl Log for CliLog {
@@ -25,19 +17,8 @@ impl Log for CliLog {
         if record.level() <= log::max_level() {
             match record.level() {
                 Level::Error => eprintln!("error: {}", record.args()),
-                Level::Warn => {
-                    if self.always_stderr {
-                        eprintln!("warn: {}", record.args());
-                    } else {
-                        println!("{}", record.args());
-                    }
-                }
-                Level::Info => {
-                    if self.always_stderr {
-                        eprintln!("{}", record.args());
-                    } else {
-                        println!("{}", record.args());
-                    }
+                Level::Info | Level::Warn => {
+                    println!("{}", record.args());
                 }
                 Level::Debug => eprintln!("debug: {}", record.args()),
                 Level::Trace => eprintln!("trace: {}", record.args()),
@@ -56,12 +37,5 @@ impl Log for CliLog {
 #[inline]
 pub fn init_with_level(filter: LevelFilter) {
     log::set_logger(&LOGGER).expect("Setup logger");
-    log::set_max_level(filter);
-}
-
-/// Initialize a logger printing to `STDERR` with specified `LevelFilter`
-#[inline]
-pub fn init_stderr_with_level(filter: LevelFilter) {
-    log::set_logger(&STDERR_LOGGER).expect("Setup logger");
     log::set_max_level(filter);
 }
